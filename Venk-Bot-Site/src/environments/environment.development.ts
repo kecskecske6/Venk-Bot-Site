c4e1867a8755afbcf67b58b218fe6414878fd043
Tankovits Bence <btankovits@n-ware.eu>
2023-11-06 13:25:02 +0100

    Minor changes

--- Venk-Bot-Site/src/environments/environment.development.ts
+++ Venk-Bot-Site/src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  backend: 'https://venkbot-6ab10a8ce669.herokuapp.com/api'
+  backend: 'http://localhost:8080/api'
 };
--- Venk-Bot-Site/src/environments/environment.ts
+++ Venk-Bot-Site/src/environments/environment.ts
@@ -1,1 +1,3 @@
-export const environment = {};
+export const environment = {
+  backend: 'https://venkbot-6ab10a8ce669.herokuapp.com/api'
+};
