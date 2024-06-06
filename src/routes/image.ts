a52e93ccb1bbe0cb67a7cf1a73c2bfd77e450ddb
kecskecske6 <kecskecske6@gmail.com>
2024-06-06 20:56:38 +0200

    Added image route

---
+++ src/routes/image.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express();
+
+router.get('', (req, res) => {
+    res.redirect('https://boobsywteiahkrhvuryb.supabase.co/storage/v1/object/public/venk-site/jonk.png');
+});
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import user from './user';
 import mod from './mod';
 import game from './game';
+import image from './image';
 
 const router = express();
 
 router.use('/user', user);
 router.use('/mod', mod);
 router.use('/game', game);
+router.use('/image', image);
 
 export default router;
